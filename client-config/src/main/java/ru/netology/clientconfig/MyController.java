0d8308e19f56e83c0c79c143063711814beab4a3
Kseniya G <gashchenko549@gmail.com>
2026-04-10 22:23:26 +0400

    Spring Cloud Config homework: server, client and config

---
+++ client-config/src/main/java/ru/netology/clientconfig/MyController.java
@@ -0,0 +1,17 @@
+package ru.netology.clientconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+    @Value("${app.message}")
+    private String message;
+
+    @GetMapping("/")
+    public String hello() {
+        return message;
+    }
+}
+
---
+++ client-config/src/test/java/ru/netology/clientconfig/ClientConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.clientconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
